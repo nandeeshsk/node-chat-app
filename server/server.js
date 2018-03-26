d21cbf6f538bbdb4570e0b366824d3b04b638a5a
Nandeesh Kulkarni <knandeesh-1@TOSHIBA-TSIP.COM>
2018-03-26 15:07:02 +0530

    send and recieve messages to all the clients

--- server/server.js
+++ server/server.js
@@ -12,23 +12,14 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
   console.log('New User connected');
 
-  socket.emit('newEmail',{
-    from : 'abc@gmail.com',
-    text:'hello world',
-    createdAt: '123'
-  });
 
-  socket.on('createEmail',(newEmail)=>{
-    console.log('crearedEmail  :',newEmail);
-  });
-
-  socket.emit('newMessage',{
-    from:'Nandeesh',
-    text : 'Hello Welcome how can I help you? ',
-    createdAt:'123'
-  });
   socket.on('createMessage',(message)=>{
     console.log('message from client :',message);
+    io.emit('newMessage',{
+      from:message.from,
+      text :message.text,
+      createdAt: new Date().getTime()
+    });
 
   });
   socket.on('disconnect',()=>{
@@ -39,7 +30,7 @@ io.on('connection',(socket)=>{
 
 
 
-})
+});
 
 var publicPath = path.join(__dirname,'../public');
 
